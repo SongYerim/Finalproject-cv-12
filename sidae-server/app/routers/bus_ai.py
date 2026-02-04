dd5db010574786c8de08878bbba592bafcef76ff
hanjun <hanjun0126@naver.com>
2026-02-04 18:00:26 +0900

    Fix: code error fix

--- sidae-server/app/routers/bus_ai.py
+++ sidae-server/app/routers/bus_ai.py
@@ -85,7 +85,7 @@ async def identify_bus(file: UploadFile = File(...), mode: str = Form(...), vlm_
                 mode = '하차벨'
             elif mode == 'tag':
                 mode ='태그기'
-            if pos == ["카드 단말기 없음", "하차벨 없음"]:
+            if pos in ["카드 단말기 없음", "하차벨 없음"]:
                 des = f'{mode} 위치를 못 찾겠습니다.'
             else:
                 des = f'{mode} 위치는 {pos}에 있습니다.'
